d9a760fe011a7a655483a03f5b3e6182d611c8e2
JasonCheng1 <stealthyjason@gmail.com>
2021-04-07 18:20:24 -0400

    initial cache

--- my_server.py
+++ my_server.py
@@ -8,14 +8,42 @@ import argparse
 from sys import argv
 from time import sleep
 from helper_funcs import DNSQuery
+from datetime import datetime, timedelta
+from collections import OrderedDict
 
-MAX_LEVEL = 10 # max number of iterative queries we make before we stop
+MAX_LEVEL = 10  # max number of iterative queries we make before we stop
+CACHE_SIZE = 10000
+
+
+class LRUCache:
+    def __init__(self, Capacity):
+        self.size = Capacity
+        self.cache = OrderedDict()
+
+    def get(self, key):
+        if key not in self.cache:
+            return -1
+        val = self.cache[key]
+        self.cache.move_to_end(key)
+        return val
+
+    def put(self, key, val):
+        if key in self.cache:
+            del self.cache[key]
+        self.cache[key] = val
+        if len(self.cache) > self.size:
+            self.cache.popitem(last=False)
+
 
 class MyResolver(DnsResolver):
     def __init__(self, port):
         self.port = port
         # define variables and locks you will need here
         self.cache_lock = threading.Lock()
+        self.cache = (
+            {}
+        )  # TODO Not sure if there is a size limit on this if so then what process do I use to remove items in cache
+        # self.cache = LRUCache(CACHE_SIZE)
 
     def get_dns_response(self, query):
         # input: A query and any state in self
@@ -33,90 +61,77 @@ class MyResolver(DnsResolver):
             ### EDNS
             # Reference: https://tools.ietf.org/html/rfc6891#section-6
             q.header["QR"] = 1
-            q.header["RCODE"] = 2  # /3/4 Not sure if this is the right one or is this the only thing I need to do
+            q.header["RCODE"] = 4
             q.header["ANCOUNT"] = 0
             q.header["NSCOUNT"] = 0
             q.header["ARCOUNT"] = 0
             q.answers = []
             return q.to_bytes()
 
-        """
-        ### PseudoCode ###
-        # DNS Functions: https://tools.ietf.org/html/rfc1034#section-5.2.1
-        # host name -> host address  <- implementing this one
-        # host address -> host name (inverse query)
-        # general lookup 
+        sname, stype, sclass = (
+            q.question["NAME"].decode("utf-8"),
+            q.question["QTYPE"],
+            q.question["QCLASS"],
+        )
+        key = (sname, stype, sclass)
+        now = datetime.now()
+        if key in self.cache and self.cache[key]["expire_time"] > now:
+            q.header["QR"] = 1
+            q.header["ANCOUNT"] = 1
+            q.header["AA"] = 0  # Because result is from a cache
+            # a = self.cache.get(key)  # ["resp"]
+            a = self.cache[key]["resp"]
 
-        # Algo: https://tools.ietf.org/html/rfc1034#section-4.3.2
-        sname, stype, sclass = q.question["NAME"], q.header["QTYPE"], q.header["QCLASS"]
-        slist = []
-        sbelt = [] # initialize this using a configuration file use a couple of the root servres
-        # q.header["RD"] = 0 #no recursive queries
-        ns = "198.41.0.4" # #13 root servers https://www.internic.net/domain/named.root
-        
-        if (sname, stype, sclass) in self.cache_lock and not expired:
-            return "we have our answer put into a dns response"
+            a["TTL"] = int(
+                (self.cache[key]["expire_time"] - now).total_seconds()
+            )  # NOTE because "a" is a reference and not a copy so the cache object will change
+            q.answers.append(a)
+            return q.to_bytes()
 
-        # www.google.com. -> google.com. -> com. -> ""
-        while qname:
-            if (sname, stype, sclass) in self.cache_lock and not expired:
-                    slist.append(self.cache_lock[(sname, stype, sclass)]["ns"])
-            qname = qname[qname.find(".")+1:] 
-            slist.reverse()
-            #slist is now in the form of wrose -> best
-        
+        else:
+            ### Regular Query
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.bind(("", 7000))
+            sock.connect(("8.8.8.8", 53))
+            sock.send(q.to_bytes())
+            answer = sock.recv(512)
+            a = DNSQuery(answer)
+            # TODO Make this into a method so that we cache every time the resolver fetches during iterative query
+            for record in a.answers:
+                # if record["TYPE"]
+                key = (record["NAME"].decode("utf-8"), record["TYPE"], record["CLASS"])
+                with self.cache_lock:
+                    self.cache[key] = {
+                        "insert_time": datetime.now(),
+                        "expire_time": datetime.now()
+                        + timedelta(seconds=record["TTL"]),
+                        "resp": record,
+                    }
+            print("%%")
+            print([[str(num) for num in record["RDATA"]] for record in a.answers])
+            print(a)
+            # print(a.to_bytes())
+            return a.to_bytes()
 
-        def recursive_lookup(qname: str,  ns: str, qtype: str = "A",  qclass: str = "1"): 
-            while True:
-                server = (ns, 53)
-                resp = self.lookup(qname, server) #create socket -> send dns query with qname in question
-                if resp.answers and resp.header["RCODE"] == 0: # 0 = NOERROR:
 
-                    return resp #we got the ans
-                
-                if resp.header["RCODE"] == 3: # 3 = NXDOMAIN
-                    return resp #qname DNE
+# def lookup(self, qname: str, ns):
+#     """
+#     setups and sends a dns query to a name server
 
-                if new_ns = resp.
+#     returns query
+#     """
+# 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#     sock.bind((ns, 7000))
+#     sock.connect((, 53))
+#     sock.send(q.to_bytes())
+#     answer = sock.recv(512)
+#     a = DNSQuery(answer)
+#     print("%%")
+#     print([[str(num) for num in record["RDATA"]] for record in a.answers])
+#     print(a)
+#     # print(a.to_bytes())
+#     return a.to_bytes()
 
-        for ns in slist:
-            recursive_lookup(...)
-
-        #if slist is null or we can't find ans yet
-        for ns in sbelt:
-            recursive_lookup(...)
-        """
-        ### Regular Query
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.bind(("", 7000))
-        sock.connect(("8.8.8.8", 53))
-        sock.send(q.to_bytes())
-        answer = sock.recv(512)
-        a = DNSQuery(answer)
-        print("%%")
-        print([[str(num) for num in record["RDATA"]] for record in a.answers])
-        print(a)
-        # print(a.to_bytes())
-        return a.to_bytes()
-
-	def lookup(self, qname: str, ns):
-        """
-        setups and sends a dns query to a name server 
-
-        returns query
-        """
-		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.bind((ns, 7000))
-        sock.connect((, 53))
-        sock.send(q.to_bytes())
-        answer = sock.recv(512)
-        a = DNSQuery(answer)
-        print("%%")
-        print([[str(num) for num in record["RDATA"]] for record in a.answers])
-        print(a)
-        # print(a.to_bytes())
-        return a.to_bytes()
-		
 
 parser = argparse.ArgumentParser(description="""This is a DNS resolver""")
 parser.add_argument(
@@ -128,6 +143,4 @@ parser.add_argument(
 args = parser.parse_args(argv[1:])
 resolver = MyResolver(args.port)
 resolver.wait_for_requests()
-
-
 
