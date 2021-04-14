b01fe7c08baee9101d191c0ec554fb555e8ff764
JasonCheng1 <stealthyjason@gmail.com>
2021-04-14 19:25:45 -0400

    Dns Resolver Algo Pt 1

--- my_server.py
+++ my_server.py
@@ -29,11 +29,12 @@ class LRUCache:
         return val
 
     def put(self, key, val):
-        if key in self.cache:
-            del self.cache[key]
-        self.cache[key] = val
-        if len(self.cache) > self.size:
-            self.cache.popitem(last=False)
+        with self.cache_lock:
+            if key in self.cache:
+                del self.cache[key]
+            self.cache[key] = val
+            if len(self.cache) > self.size:
+                self.cache.popitem(last=False)
 
 
 class MyResolver(DnsResolver):
@@ -42,6 +43,47 @@ class MyResolver(DnsResolver):
         # define variables and locks you will need here
         self.cache_lock = threading.Lock()
         self.cache = LRUCache(CACHE_SIZE)
+
+    def check_Cache(self, key, now):
+        a = self.cache.get(key)
+        if a:
+            a = [record for record in a if record["expire_time"] > now]
+            # Get rid of expire records
+            self.cache.put(key, a if a else None)
+
+    def query_then_cache(self, name_server, q):
+        ### Regular Query
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind(("", 7000))
+        sock.connect(
+            (name_server, 53)
+        )  # Dutch Dns Server that supports non recursive queries
+        q.header["RD"] = 0  # we do not want recursive query
+        sock.send(q.to_bytes())
+        answer = sock.recv(512)
+        a = DNSQuery(answer)
+        # TODO Make this into a method so that we cache every time the resolver fetches during iterative query
+
+        new_rr = defaultdict(list)
+        for record in a.answers:
+            # TODO if record["TYPE"] # Are certain types like SOA we don't cache
+            key = (
+                record["NAME"].decode("utf-8"),
+                record["TYPE"],
+                record["CLASS"],
+            )
+            val = {
+                "expire_time": datetime.now() + timedelta(seconds=record["TTL"]),
+                "resp": record,
+            }
+            new_rr[key].append(val)
+        for key, val in new_rr.items():
+            self.cache.put(key, val)
+        print("%%")
+        print([[str(num) for num in record["RDATA"]] for record in a.answers])
+        print(a)
+        return a
+        # return a.to_bytes()
 
     def get_dns_response(self, query):
         # input: A query and any state in self
@@ -71,53 +113,75 @@ class MyResolver(DnsResolver):
             q.question["QTYPE"],
             q.question["QCLASS"],
         )
-        key = (sname, stype, sclass)
-        now = datetime.now()
-        a = self.cache.get(key)
 
-        if a:
-            a = [record for record in a if record["expire_time"] > now]
-            with self.cache_lock:  # Get rid of expire records
-                self.cache.put(key, a if a else None)
-        if a and any(True for record in a if record["expire_time"] > now):
-            q.header["QR"] = 1
-            q.header["ANCOUNT"] = len(a)
-            q.header["AA"] = 0  # Because result is from a cache
-            for record in a:
-                record["resp"]["TTL"] = int(
-                    (record["expire_time"] - now).total_seconds()
-                )  # NOTE because "a" is a reference and not a copy so the cache object will change
+        def recursive_lookup(sname, stype, sclass, now=datetime.now()):
+            if (
+                datetime.now() - now > 100
+            ):  # look up took longer than 100s we should return an error
+                q.header["QR"] = 1
+                q.header["RCODE"] = 2
+                q.header["ANCOUNT"] = 0
+                q.header["NSCOUNT"] = 0
+                q.header["ARCOUNT"] = 0
+                q.answers = []
+                return q.to_bytes()
 
-            q.answers.extend([r["resp"] for r in a])
-            return q.to_bytes()
+            key = (sname, stype, sclass)
+            ### STEP 1 ###
+            a = self.check_Cache(key, now)
+            if a and any(True for record in a if record["expire_time"] > now):
+                q.header["QR"] = 1
+                q.header["ANCOUNT"] = len(a)
+                q.header["AA"] = 0  # Because result is from a cache
+                for record in a:
+                    record["resp"]["TTL"] = int(
+                        (record["expire_time"] - now).total_seconds()
+                    )  # NOTE because "a" is a reference and not a copy so the cache object will change
 
-        else:
-            ### Regular Query
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            sock.bind(("", 7000))
-            sock.connect(("8.8.8.8", 53))
-            sock.send(q.to_bytes())
-            answer = sock.recv(512)
-            a = DNSQuery(answer)
-            # TODO Make this into a method so that we cache every time the resolver fetches during iterative query
+                q.answers.extend([r["resp"] for r in a])
+                return q.to_bytes()
 
-            new_rr = defaultdict(list)
-            for record in a.answers:
-                # TODO if record["TYPE"] # Are certain types like SOA we don't cache
-                key = (record["NAME"].decode("utf-8"), record["TYPE"], record["CLASS"])
-                val = {
-                    "expire_time": datetime.now() + timedelta(seconds=record["TTL"]),
-                    "resp": record,
-                }
-                new_rr[key].append(val)
-            with self.cache_lock:
-                for key, val in new_rr.items():
-                    self.cache.put(key, val)
-            print("%%")
-            print([[str(num) for num in record["RDATA"]] for record in a.answers])
-            print(a)
-            # print(a.to_bytes())
-            return a.to_bytes()
+            while True:
+
+                ### STEP 2 ###
+                sbelt = ["195.129.12.83", "199.9.14.201"]  # A and B root server
+                slist = []
+                while sname:
+                    key = (sname, stype, sclass)
+                    if a := self.check_Cache(key, now):
+                        slist.append(a["RDATA"])  # put the ip address of the server
+                slist.extend(sbelt)
+
+                ### STEP 3 ###
+                for server in slist:
+
+                    ### STEP 4 ###
+                    a = self.query_then_cache(server, q)
+
+                    ### STEP 4.1 ###
+
+                    if (a.answers and a.header["RCODE"] == 0) or a.header[
+                        "RCODE"
+                    ] == 3:  # TODO Maybe we need to check if domain name and type of answer section matches with original query
+                        return a.to_bytes()
+
+                    ### STEP 4.2 ###
+                    elif a.answer and (
+                        ns_rec := [rec for rec in a.answers if rec["TYPE"] == 2]
+                    ):  # if there is an NS Record
+                        break
+
+                    ### STEP 4.3 ###
+                    elif a.answers and (
+                        cname_rec := [rec for rec in a.answers if rec["TYPE"] == 5]
+                    ):  # if there is a CNAME Record
+                        return recursive_lookup(
+                            cname_rec[0]["RDATA"], stype, sclass, now
+                        )
+
+                    ### STEP 4.4 ###
+                    # Go to the next server
+                    # continue
 
 
 # def lookup(self, qname: str, ns):
@@ -149,4 +213,3 @@ parser.add_argument(
 args = parser.parse_args(argv[1:])
 resolver = MyResolver(args.port)
 resolver.wait_for_requests()
-
